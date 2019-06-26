3020b8b65753ec4f5cb314af5d8c2d548840e668
Ashilesh Sonkusle <ashileshsonkusle1@gmail.com>
2019-06-26 12:43:56 +0530

    added navigation next changed code of rows and column for Welcome

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -9,33 +9,48 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Material(
-          //if material is not root then we get yellow lines under Text
 
-          child: Container(
-              decoration: BoxDecoration(
+        home: Scaffold(
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+
+          floatingActionButton: FloatingActionButton(
+           onPressed: null,
+           backgroundColor: Colors.grey[700],
+           foregroundColor: Colors.grey[50],
+              child: Icon(
+                Icons.navigate_next,
+              ),
+           ),
+
+             body: Container(
+
+               decoration: BoxDecoration(
                   image: DecorationImage(
-                      image: AssetImage('assets/photos/beach_2.jpg'),
-                      fit: BoxFit.cover)),
-              child: SafeArea(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Expanded(
-                      child: Text(
-                        'Welcome!',
-                        style: TextStyle(
-                            fontSize: 40,
-                            color: Colors.grey[600],
-                            fontFamily: "OpenSans"
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              )
-          ),
-        )
-    );
+                    image: AssetImage('assets/photos/beach_2.jpg'),
+                  fit: BoxFit.cover)),
+                child: SafeArea(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+
+                        Row(
+                           children: <Widget>[
+                             Expanded(
+                               child: Text(
+                                 'Welcome!',
+                                 textAlign: TextAlign.center,
+                                 style: TextStyle(
+                                     fontSize: 40,
+                                     color: Colors.grey[600],
+                                     fontFamily: "OpenSans"),
+                               ),
+                             )
+                           ],
+                         )
+
+              ],
+            ),
+          )),
+    ));
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:money_control/welcome.dart';
 import 'package:money_control/main.dart';
 
 void main() {
