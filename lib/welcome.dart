71832a8256cb231457218837dd8ef863a4fc709c
Ashilesh Sonkusle <ashileshsonkusle1@gmail.com>
2019-07-01 03:00:21 +0530

    added textfield to welcome page

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -6,13 +6,18 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+        theme: ThemeData(
+          // to give colors to theme
+          primaryColor: Colors.grey[600]
+        ),
         home: Scaffold(
           floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
 
+          // floating action button
           floatingActionButton: FloatingActionButton(
            onPressed: null,
            backgroundColor: Colors.grey[700],
@@ -32,7 +37,9 @@ class MyApp extends StatelessWidget {
                   child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: <Widget>[
+                        Spacer(flex: 1),
 
+                        // Welcome text
                         Row(
                            children: <Widget>[
                              Expanded(
@@ -46,7 +53,43 @@ class MyApp extends StatelessWidget {
                                ),
                              )
                            ],
-                         )
+                         ),
+                        Spacer(flex: 1),
+
+                        // textfield
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Container(
+                                padding: EdgeInsets.fromLTRB(22, 0, 22, 0),
+
+                                child: TextField(
+                                  textInputAction: TextInputAction.done,
+                                  textCapitalization: TextCapitalization.sentences,
+                                  style:  TextStyle(
+                                      fontFamily: 'OpenSans',
+                                      fontSize: 25,
+
+                                  ),
+                                  cursorColor: Colors.grey[600  ],
+
+
+
+                                  decoration: InputDecoration(
+                                    border: UnderlineInputBorder(
+                                      borderRadius: BorderRadius.circular(1),
+                                    ),
+
+                                      labelText: 'Name',
+
+                                      contentPadding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        Spacer(flex: 10,)
 
               ],
             ),
